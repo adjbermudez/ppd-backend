8426617558b2c00ebf26353bc0c3dccdd1b73ce2
Deimian Vásquez <deimianvasquez@gmail.com>
2023-04-18 01:01:01 -0400

    add route delete innovation

--- src/api/routes.py
+++ src/api/routes.py
@@ -469,17 +469,18 @@ def get_innovations():
 
 
 @api.route("/innovation", methods=["POST"])
+@jwt_required()
 def create_innovation():
   if request.method == "POST":
     data_files = request.files
     data_form = request.form
-
+   
     data ={
-      'title':data_form.get('title'),
-      'url_file':data_form.get('url_file'),
-      'url_imgage':data_files.get('image'),
+      'title':data_form.get('description'),
+      'url_file':data_form.get('nameFile'),
+      'url_imgage':data_files.get('innovationImage'),
     }
-
+  
     if data is None:
       return jsonify({'message':'Bad request'}), 400
     if data.get('title') is None:
@@ -500,4 +501,27 @@ def create_innovation():
       uploader.destroy(data['public_id'])
       return jsonify({'message':'Error try again later'}), 500
     return jsonify(), 500
+  return jsonify({'message':'method not allowed'}),405
+
+
+@api.route("/innovation/<int:id>", methods=["DELETE"])
+@jwt_required()
+def delete_innovation (id=None):
+  if request.method == "DELETE":
+    if id is None:
+      return jsonify({'message':'Bad request'}), 400
+
+    innovation = Innovation.query.get(id)
+
+    if innovation is None:
+      return jsonify({'message':'Innovation not found'}), 404
+    
+    try:
+      db.session.delete(innovation)
+      db.session.commit()
+      uploader.destroy(innovation.public_id)
+      return jsonify({'message':'Innovation deleted'}), 204
+    except Exception as error:
+      print(error.args)
+      return jsonify({'message':'Error try again later'}), 500
   return jsonify({'message':'method not allowed'}),405
